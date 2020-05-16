6b170679f6e9cd0814dc4fd7f7b3cafd871e8649
Peter Trotter <patrotter@hotmail.com>
2020-05-16 12:48:40 -0500

    Initial creation

---
+++ src/main/java/com/capstone/restservice/RestServiceApplication.java
@@ -0,0 +1,14 @@
+package com.capstone.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Trigger build four
+@SpringBootApplication
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+}
